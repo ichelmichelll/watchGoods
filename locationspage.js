a0be7f22d8ab9d8030dce2454012d2e32ffb7c4d
ichelmichelll <118976125+ichelmichelll@users.noreply.github.com>
2023-05-23 16:03:22 +0700

    Add files via upload

---
+++ locationspage.js
@@ -0,0 +1,20 @@
+const showImageButton = document.getElementById("show-image-button");
+      const myImage1 = document.getElementById("myImage1");
+      showImageButton.addEventListener("click", () => 
+      { 
+        myImage1.classList.toggle("visible"); 
+      });
+
+const showImageButton1 = document.getElementById("show-image-button-1");
+        const myImage2 = document.getElementById("myImage2");
+        showImageButton1.addEventListener("click", () => 
+        { 
+        myImage2.classList.toggle("visible"); 
+        });
+
+const showImageButton2 = document.getElementById("show-image-button-2");
+        const myImage3 = document.getElementById("myImage3");
+        showImageButton2.addEventListener("click", () => 
+        { 
+        myImage3.classList.toggle("visible"); 
+        });
